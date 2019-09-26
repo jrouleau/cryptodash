05163158e91940c6637badffc2ea5c6a06db3718
Jonathan Rouleau <jonathan@rouleau.io>
2019-09-26 12:37:09 -0600

    Style favourites

--- src/components/favourites/favourites.tsx
+++ src/components/favourites/favourites.tsx
@@ -1,31 +1,58 @@
 import React from 'react'
 
-import { A } from 'hookrouter'
+import { navigate } from 'hookrouter'
+
+import { Button, Grid, Image } from 'semantic-ui-react'
 
 /**********************************************************************/
 
 const Favourites: React.FC = () => {
+  const selected = 'BTC'
   const favourites = [
-    ['BTC', 'USD'],
-    ['ETH', 'USD'],
-    ['XRP', 'USD'],
-    ['LTC', 'USD'],
-    ['DASH', 'USD'],
-    ['XMR', 'USD'],
-    ['MCO', 'USD'],
+    { Symbol: 'BTC', ImageUrl: '/media/19633/btc.png' },
+    { Symbol: 'ETH', ImageUrl: '/media/20646/eth_logo.png' },
+    { Symbol: 'XRP', ImageUrl: '/media/34477776/xrp.png' },
+    { Symbol: 'LTC', ImageUrl: '/media/35309662/ltc.png' },
+    { Symbol: 'DASH', ImageUrl: '/media/33842920/dash.png' },
+    { Symbol: 'XMR', ImageUrl: '/media/19969/xmr.png' },
+    { Symbol: 'MCO', ImageUrl: '/media/34478435/mco.png' },
   ]
 
   /* render */
   return (
-    <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
-      {favourites.map((f) => (
-        <li key={f.join(':')} style={{ display: 'inline' }}>
-          <A href={`/c/${f.join(':')}`} style={{ padding: 8 }}>
-            {f.join('/')}
-          </A>
-        </li>
-      ))}
-    </ul>
+    <Grid columns="8" doubling spacing="">
+      {favourites.map((f) => {
+        const isActive = f.Symbol === selected
+        return (
+          <Grid.Column key={f.Symbol} textAlign="center" verticalAlign="middle">
+            <Button
+              active={isActive}
+              color={isActive ? 'green' : 'blue'}
+              fluid
+              onClick={() => navigate(`/c/${f.Symbol}`)}
+              style={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Image
+                circular
+                src={`https://www.cryptocompare.com${f.ImageUrl}`}
+                style={{
+                  marginLeft: '-1rem',
+                  marginRight: '1rem',
+                  width: '2rem',
+                  height: '2rem',
+                  backgroundColor: '#fff',
+                }}
+              />
+              <span>{f.Symbol}</span>
+            </Button>
+          </Grid.Column>
+        )
+      })}
+    </Grid>
   )
 }
 
