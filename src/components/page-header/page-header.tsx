b5f1b29ff961d79d5d6569349c317bfde5346c54
Jonathan Rouleau <jonathan@rouleau.io>
2019-09-26 19:20:03 -0600

    Adjust PageHeader margin

--- src/components/page-header/page-header.tsx
+++ src/components/page-header/page-header.tsx
@@ -7,7 +7,7 @@ import { Container, Menu } from 'semantic-ui-react'
 const PageHeader: React.FC = () => {
   /* render */
   return (
-    <Container as="header" style={{ marginBottom: '2rem' }}>
+    <Container as="header" style={{ marginTop: '1rem', marginBottom: '1rem' }}>
       <Menu size="huge" secondary>
         <Menu.Item header as="h1">
           CryptoDash
