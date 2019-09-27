cba88086ae0a7035231f16fca09cc8931ce7a354
Jonathan Rouleau <jonathan@rouleau.io>
2019-09-26 19:53:01 -0600

    Add copyright and CyptoCompare to PageFooter

--- src/components/page-footer/page-footer.tsx
+++ src/components/page-footer/page-footer.tsx
@@ -6,7 +6,20 @@ import { Container } from 'semantic-ui-react'
 
 const PageFooter: React.FC = () => {
   /* render */
-  return <Container as="footer">PageFooter</Container>
+  return (
+    <Container
+      as="footer"
+      style={{ marginTop: '1rem', marginBottom: '1rem' }}
+      textAlign="center"
+    >
+      <p style={{ color: '#999' }}>
+        Copyright &copy; 2019 Jonathan Rouleau
+        <br />
+        Cryptocurrency data provided by{' '}
+        <a href="https://www.cryptocompare.com/">CryptoCompare</a>
+      </p>
+    </Container>
+  )
 }
 
 export default PageFooter
